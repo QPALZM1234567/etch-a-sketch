8dd9364897a427386d9fbc8065030cfa713ad428
Sean <seanjung03@gmail.com>
2022-07-28 15:58:05 -0400

    Add buttons and make clear button work

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Add squares to board
 const container = document.querySelector(".container");
 for (let i = 0; i < 256; i++) {
     let div = document.createElement('div');
@@ -6,7 +7,21 @@ for (let i = 0; i < 256; i++) {
     div.classList.add("square");
 }
 
+//Clears board
+const clear = document.querySelector(".clear");
+clear.addEventListener('click', () => {
+    /*
+    document.querySelectorAll(".hovered").forEach((child) =>{
+        child.classList.remove("hovered");
+    });
+    */
+   for (child of document.querySelectorAll(".hovered")) {
+    child.classList.remove("hovered");
+   }
+})
 
+
+//Create Functionality for board
 function hovered(event) {
     event.target.classList.add("hovered");
 }
@@ -14,10 +29,12 @@ const squares = document.querySelectorAll(".square");
 
 squares.forEach(item => {
     item.addEventListener('mouseover', hovered);
+    /*
     item.addEventListener('mouseout', (e) =>{
         setTimeout(() => {
             e.target.classList.remove("hovered");
         }, 300)
         
     });
+    */
 })
